e2ffbb488b0d8f9f0db983700f64d676adf9fc5e
RichardTorresRivera <rmtr2711@gmail.com>
2025-10-09 14:41:12 -0500

    add native properties to components

--- src/components/FileAction.tsx
+++ src/components/FileAction.tsx
@@ -1,29 +1,42 @@
-import { Upload, Download } from "lucide-react";
+import { Upload, Download, RefreshCw, type LucideIcon } from "lucide-react";
 
 type FileActionProps = {
   text: string;
-  variant: "upload" | "download";
+  variant: "upload" | "download" | "update" | "other";
   onClick?: () => void;
+  icon?: LucideIcon;
 };
 
-const FileAction = ({ text, variant, onClick }: FileActionProps) => {
-  const isUpload = variant === "upload";
+const FileAction = ({
+  text,
+  variant,
+  onClick,
+  icon: Icon,
+}: FileActionProps) => {
+  const getIcon = () => {
+    switch (variant) {
+      case "upload":
+        return <Upload className="h-5 w-5" />;
+      case "download":
+        return <Download className="h-5 w-5" />;
+      case "update":
+        return <RefreshCw className="h-5 w-5" />;
+      case "other":
+        return Icon ? <Icon className="h-5 w-5" /> : null;
+      default:
+        return null;
+    }
+  };
 
   return (
     <button
       type="button"
       aria-label={text}
       onClick={onClick}
-      className={
-        "cursor-pointer flex items-center text-sm gap-2 text-body-color underline hover:text-secondary-color"
-      }
+      className="cursor-pointer flex items-center text-sm gap-2 text-body-color underline hover:text-secondary-color"
     >
       <span>{text}</span>
-      {isUpload ? (
-        <Upload className="h-5 w-5" />
-      ) : (
-        <Download className="h-5 w-5" />
-      )}
+      {getIcon()}
     </button>
   );
 };
