f03085d36ff65dbfc460d32060ce6afed8419eb7
FranzARCH <franzchipana2610@gmail.com>
2025-11-14 23:29:15 -0500

    Create PromocionesPage

---
+++ src/pages/catalogo/PromocionesPage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PromocionesPage: React.FC = () => {
+  return (
+    <div className="p-6 min-h-screen">
+      <div className="mb-6">
+        <h2 className="text-2xl font-semibold mb-2 text-[#363636]">
+          Gestión de Promociones
+        </h2>
+      </div>
+
+    </div>
+  );
+};
+
+export default PromocionesPage;
